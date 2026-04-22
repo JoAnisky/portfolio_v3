fb7881d1790d6959b105e98488b63ca4fb2ed84a
Jonathan Loré <aniskydev@protonmail.com>
2026-04-22 14:38:25 +0200

    [ADD] ProjectCrudController :  "Gérer les screenshots" Button

--- src/Controller/Admin/ProjectCrudController.php
+++ src/Controller/Admin/ProjectCrudController.php
@@ -12,14 +12,21 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Provider\AdminContextProvider;
 
 class ProjectCrudController extends AbstractCrudController
 {
+    public function __construct(
+        private AdminUrlGenerator $adminUrlGenerator,
+        private AdminContextProvider $adminContextProvider,
+    ) {}
+
     public static function getEntityFqcn(): string
     {
         return Project::class;
@@ -36,6 +43,31 @@ class ProjectCrudController extends AbstractCrudController
     {
         yield IdField::new('id')->hideOnForm();
         yield TextField::new('name', 'Nom');
+
+        // Ajoute un lien "Gérer les screenshots" : Ouvre une nouvelle page des screenshots liés au projet
+        if ($pageName === Crud::PAGE_EDIT) {
+            $entity = $this->adminContextProvider->getContext()?->getEntity()->getInstance();
+
+            $url = $this->adminUrlGenerator
+                ->setController(ScreenshotCrudController::class)
+                ->setAction('index')
+                ->set('filters[project][comparison]', '=')
+                ->set('filters[project][value]', (string) $entity->getId())
+                ->generateUrl();
+
+            yield Field::new('screenshots_link', false)
+                ->setFormTypeOption('mapped', false)
+                ->setFormTypeOption('required', false)
+                ->setFormTypeOption('attr', ['style' => 'display:none'])
+                ->setHelp(sprintf(
+                    '<a href="%s" class="btn btn-sm btn-secondary" target="_blank">
+                                <i class="fa fa-image"></i> Gérer les screenshots
+                            </a>',
+                    $url
+                ))
+                ->onlyWhenUpdating();
+        }
+
         yield DateField::new('date', 'Date');
         yield ChoiceField::new('context', 'Contexte')
             ->setChoices([
