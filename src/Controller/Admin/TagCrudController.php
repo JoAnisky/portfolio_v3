c41c591f3524a08868c8ccac8b039b86e5f5b074
Jonathan Loré <aniskydev@protonmail.com>
2026-05-01 15:58:51 +0200

    [UPDATED] Tag icon & color : TagCrudController

--- src/Controller/Admin/TagCrudController.php
+++ src/Controller/Admin/TagCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Tag;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ColorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -18,5 +19,8 @@ class TagCrudController extends AbstractCrudController
     {
         yield IdField::new('id')->hideOnForm()->hideOnIndex();
         yield TextField::new('name', 'Nom');
+        yield TextField::new('icon', 'Icon filename')->setHelp('Ex : vitrine, sass, eshop, tool...');
+        yield ColorField::new('color', 'Couleur')->setRequired(false);
+
     }
 }
